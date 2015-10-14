53c87d6fbdb972b1af87bc760261f72dea6768c4
Navarro-321 <erich.zakopal@ivemep.cz>
2015-10-14 18:53:37 +0200

    Testy - 2015/10/14

---
+++ MyCordovaApp1/Assets/scripts/modules/core/module.ts
@@ -0,0 +1,8 @@
+﻿/// <reference path="../../../../typings/tsd.d.ts" />
+
+module MyCordovaApp1.Core {
+    'uses strict';
+
+    angular.module("MyCordovaApp1.Core", ["MyCordovaApp1.Core.Controllers", "MyCordovaApp1.Core.Services"]);
+
+}
